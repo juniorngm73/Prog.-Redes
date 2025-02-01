331b728d69cf2c139417f06918622c2f9888c2f9
juniorngm73 <juniorngm@yahoo.com.br>
2025-02-01 16:06:34 -0300

    atz

--- q2atv3.py
+++ q2atv3.py
@@ -1,15 +1,14 @@
 import socket, sys
 
-
+# ----------------------------------------------------------------------
 PORT        = 80
-CODE_PAGE   = 'utf-8'
-BUFFER_SIZE = 2048
+BUFFER_SIZE = 2048  #  Capacidade de armazenamento de dados em um buffer(Alocação Memória).
 # ----------------------------------------------------------------------
 
-host = input('\nInforme o nome do HOST ou URL do site: ')
 
+host = 'www.globo.com'
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.settimeout(5)
+tcp_socket.settimeout(10)
 
 try:
     tcp_socket.connect((host, PORT))
@@ -18,22 +17,20 @@ except:
 else:
     requisicao = f'GET / HTTP/1.1\r\nHost: {host}\r\nAccept: text/html\r\n\r\n'
     try:
-        tcp_socket.sendall(requisicao.encode(CODE_PAGE))
+        tcp_socket.sendall(requisicao.encode('ISO-8859-1'))
     except:
         print(f'\nERRO.... {sys.exc_info()[0]}')
     else:
 
 
         while True:
-            resposta = tcp_socket.recv(BUFFER_SIZE).decode(CODE_PAGE)
-            if not resposta:
+            output = tcp_socket.recv(BUFFER_SIZE)
+
+            if not output:
                     break
 
-            with open('resposta', 'w') as file:
-                file.write(resposta)
-                print(resposta)
+            with open('output.html ', 'wb') as file:
+                file.write(output)
 
-
-
-    tcp_socket.close()
+tcp_socket.close()
 
