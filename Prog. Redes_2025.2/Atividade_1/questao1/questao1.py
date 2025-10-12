f3a6c2bb51ac915975f7b58d33ed82c485336671
Natanael Gomes de Melo Junior <juniorngm@yahoo.com.br>
2025-10-12 16:43:33 -0300

    atualização
    questoes 1,2 e 3 Atividade 1

--- Prog. Redes_2025.2/Atividade_1/questao1/questao1.py
+++ Prog. Redes_2025.2/Atividade_1/questao1/questao1.py
@@ -1,1 +1,31 @@
-# Coloque aqui o código de resposta aa questao 1
+import math
+
+def soma_fatorial():
+    # Pré-calcula os fatoriais de 0 a 9 para acesso rápido
+    fatoriais = [math.factorial(i) for i in range(10)]
+
+
+    limite_superior = 2540161
+
+    soma_total = 0
+
+    for n in range(10, limite_superior):
+        soma_fatoriais_digitos = 0
+        temp_n = n
+
+        # Calcula a soma dos fatoriais dos dígitos
+        while temp_n > 0:
+            digito = temp_n % 10
+            soma_fatoriais_digitos += fatoriais[digito]
+            temp_n //= 10
+
+        # Verifica se o número é igual à soma dos fatoriais de seus dígitos
+        if n == soma_fatoriais_digitos:
+            soma_total += n
+
+    return soma_total
+
+
+
+resultado = soma_fatorial()
+print(f"A soma de todos os números que são iguais à soma do fatorial de seus dígitos é: {resultado}")
